ae762d7dc05e19685257506092ba3a21c1576554
Joca Sousa <jocasousac@gmail.com>
2025-05-26 13:51:08 -0300

    feat: configurado typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,28 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { ProducersModule } from './producers/producers.module';
 import { FarmsModule } from './farms/farms.module';
 import { SeasonsModule } from './seasons/seasons.module';
 import { CultivationModule } from './cultivation/cultivation.module';
 
 @Module({
-  imports: [ProducersModule, FarmsModule, SeasonsModule, CultivationModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      port: 5432,
+    }),
+    ProducersModule,
+    FarmsModule,
+    SeasonsModule,
+    CultivationModule,
+  ],
 })
 export class AppModule {}
